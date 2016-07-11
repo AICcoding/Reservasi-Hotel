e35c3e12a6cd3f343e7d081d5789a75edb4e2b62
josuasinaga23 <josua.sinaga@live.com>
2016-07-11 13:27:50 +0800

    update database

--- reservasi.sql
+++ reservasi.sql
@@ -1,280 +1,287 @@
-/*
-Navicat MySQL Data Transfer
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 11, 2016 at 07:26 AM
+-- Server version: 5.5.22
+-- PHP Version: 5.5.12
 
-Source Server         : mysql
-Source Server Version : 50621
-Source Host           : localhost:3306
-Source Database       : reservasi
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
-Target Server Type    : MYSQL
-Target Server Version : 50621
-File Encoding         : 65001
 
-Date: 2016-07-04 15:26:00
-*/
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
-SET FOREIGN_KEY_CHECKS=0;
+--
+-- Database: `reservasi`
+--
 
--- ----------------------------
--- Table structure for extra_bed
--- ----------------------------
-DROP TABLE IF EXISTS `extra_bed`;
-CREATE TABLE `extra_bed` (
-`id_extra_bed`  int(255) NOT NULL AUTO_INCREMENT ,
-`id_reservasi`  int(10) UNSIGNED NOT NULL ,
-`id_tarif`  int(10) UNSIGNED NULL DEFAULT NULL ,
-`tgl_sewa`  date NULL DEFAULT NULL ,
-`tgl_berhenti`  date NULL DEFAULT NULL ,
-`status_selesai`  tinyint(4) NULL DEFAULT 0 ,
-PRIMARY KEY (`id_extra_bed`),
-FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-FOREIGN KEY (`id_tarif`) REFERENCES `tarif` (`id_tarif`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `fk_id_reservasi2` (`id_reservasi`) USING BTREE ,
-INDEX `fk_id_tarif2` (`id_tarif`) USING BTREE 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=7
+-- --------------------------------------------------------
 
-;
+--
+-- Table structure for table `extra_bed`
+--
 
--- ----------------------------
--- Records of extra_bed
--- ----------------------------
-BEGIN;
-INSERT INTO `extra_bed` VALUES ('2', '1', '13', '2016-07-03', '2016-07-04', '0'), ('4', '1', '12', '2016-07-03', '2016-07-04', '0'), ('6', '1', '12', '2016-07-03', '2016-07-04', '0');
-COMMIT;
+CREATE TABLE IF NOT EXISTS `extra_bed` (
+  `id_extra_bed` int(255) NOT NULL AUTO_INCREMENT,
+  `id_reservasi` int(10) unsigned NOT NULL,
+  `id_tarif` int(10) unsigned DEFAULT NULL,
+  `tgl_sewa` date DEFAULT NULL,
+  `tgl_berhenti` date DEFAULT NULL,
+  `status_selesai` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`id_extra_bed`),
+  KEY `fk_id_reservasi2` (`id_reservasi`) USING BTREE,
+  KEY `fk_id_tarif2` (`id_tarif`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
--- ----------------------------
--- Table structure for kamar
--- ----------------------------
-DROP TABLE IF EXISTS `kamar`;
-CREATE TABLE `kamar` (
-`id_kamar`  int(255) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`id_tarif`  int(10) UNSIGNED NULL DEFAULT NULL ,
-`fasilitas`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-PRIMARY KEY (`id_kamar`),
-FOREIGN KEY (`id_tarif`) REFERENCES `tarif` (`id_tarif`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `fk_id_tarif` (`id_tarif`) USING BTREE 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=5
+--
+-- Dumping data for table `extra_bed`
+--
 
-;
+INSERT INTO `extra_bed` (`id_extra_bed`, `id_reservasi`, `id_tarif`, `tgl_sewa`, `tgl_berhenti`, `status_selesai`) VALUES
+(2, 1, 13, '2016-07-03', '2016-07-04', 0),
+(4, 1, 12, '2016-07-03', '2016-07-04', 0),
+(6, 1, 12, '2016-07-03', '2016-07-04', 0);
 
--- ----------------------------
--- Records of kamar
--- ----------------------------
-BEGIN;
-INSERT INTO `kamar` VALUES ('1', '13', 'TV LED, Wifi, AC'), ('2', '14', 'TV LED, Wifi, AC, Air hangat, Kulkas'), ('4', '14', 'TV LED, Wifi, AC, Air hangat, Kulkas');
-COMMIT;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Table structure for konsumsi
--- ----------------------------
-DROP TABLE IF EXISTS `konsumsi`;
-CREATE TABLE `konsumsi` (
-`id_konsumsi`  int(255) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`nama`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`harga`  int(10) UNSIGNED NULL DEFAULT NULL ,
-PRIMARY KEY (`id_konsumsi`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=3
+--
+-- Table structure for table `kamar`
+--
 
-;
+CREATE TABLE IF NOT EXISTS `kamar` (
+  `id_kamar` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `lantai` tinyint(4) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `id_tarif` int(10) unsigned DEFAULT NULL,
+  `fasilitas` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id_kamar`),
+  KEY `fk_id_tarif` (`id_tarif`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=204 ;
 
--- ----------------------------
--- Records of konsumsi
--- ----------------------------
-BEGIN;
-INSERT INTO `konsumsi` VALUES ('1', 'Aqua gelas', '1000'), ('2', 'Aqua tanggung', '4000');
-COMMIT;
+--
+-- Dumping data for table `kamar`
+--
 
--- ----------------------------
--- Table structure for log_reservasi
--- ----------------------------
-DROP TABLE IF EXISTS `log_reservasi`;
-CREATE TABLE `log_reservasi` (
-`id_reservasi`  int(10) UNSIGNED NOT NULL ,
-`id_konsumsi`  int(10) UNSIGNED NULL DEFAULT NULL ,
-`qty`  int(11) NULL DEFAULT NULL ,
-`custom`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`tarif`  int(10) UNSIGNED NULL DEFAULT NULL ,
-FOREIGN KEY (`id_konsumsi`) REFERENCES `konsumsi` (`id_konsumsi`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `fk_id_reservasi3` (`id_reservasi`) USING BTREE ,
-INDEX `fk_id_konsumsi` (`id_konsumsi`) USING BTREE 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+INSERT INTO `kamar` (`id_kamar`, `lantai`, `type`, `id_tarif`, `fasilitas`) VALUES
+(1, 0, '', 13, 'TV LED, Wifi, AC'),
+(2, 0, '', 14, 'TV LED, Wifi, AC, Air hangat, Kulkas'),
+(4, 0, '', 14, 'TV LED, Wifi, AC, Air hangat, Kulkas'),
+(101, 1, 'Single', NULL, NULL),
+(102, 1, 'Double', NULL, NULL),
+(103, 1, 'Single', NULL, NULL),
+(201, 2, 'Balinese Single', NULL, NULL),
+(202, 2, 'Triple', NULL, NULL),
+(203, 2, 'Single', NULL, NULL);
 
-;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Records of log_reservasi
--- ----------------------------
-BEGIN;
-COMMIT;
+--
+-- Table structure for table `konsumsi`
+--
 
--- ----------------------------
--- Table structure for reservasi
--- ----------------------------
-DROP TABLE IF EXISTS `reservasi`;
-CREATE TABLE `reservasi` (
-`id_reservasi`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`id_kamar`  int(10) UNSIGNED NULL DEFAULT NULL ,
-`tgl_check_in`  date NULL DEFAULT NULL ,
-`jam_check_in`  time NULL DEFAULT NULL ,
-`tgl_check_out`  date NULL DEFAULT NULL ,
-`jam_check_out`  time NULL DEFAULT NULL ,
-`temp_bayar`  int(11) NULL DEFAULT 0 ,
-`status_out`  tinyint(4) NULL DEFAULT 0 ,
-PRIMARY KEY (`id_reservasi`),
-FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `fk_id_kamar` (`id_kamar`) USING BTREE 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=3
+CREATE TABLE IF NOT EXISTS `konsumsi` (
+  `id_konsumsi` int(255) unsigned NOT NULL AUTO_INCREMENT,
+  `nama` varchar(255) DEFAULT NULL,
+  `harga` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_konsumsi`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
-;
+--
+-- Dumping data for table `konsumsi`
+--
 
--- ----------------------------
--- Records of reservasi
--- ----------------------------
-BEGIN;
-INSERT INTO `reservasi` VALUES ('1', '2', '2016-07-03', '16:55:38', '0000-00-00', '16:55:45', '0', '0'), ('2', '1', '2016-07-04', '14:24:34', null, null, '0', '0');
-COMMIT;
+INSERT INTO `konsumsi` (`id_konsumsi`, `nama`, `harga`) VALUES
+(1, 'Aqua gelas', 1000),
+(2, 'Aqua tanggung', 4000);
 
--- ----------------------------
--- Table structure for tamu
--- ----------------------------
-DROP TABLE IF EXISTS `tamu`;
-CREATE TABLE `tamu` (
-`id_tamu`  varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL ,
-`nama`  varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`alamat`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`telepon`  varchar(20) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-PRIMARY KEY (`id_tamu`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
+-- --------------------------------------------------------
 
-;
+--
+-- Table structure for table `log_reservasi`
+--
 
--- ----------------------------
--- Records of tamu
--- ----------------------------
-BEGIN;
-INSERT INTO `tamu` VALUES ('12', 'agus', 'klungkung city', '081'), ('1313', 'Agus suarya', 'Klungkung', '085'), ('1334', 'Putu', 'Bali', '087');
-COMMIT;
+CREATE TABLE IF NOT EXISTS `log_reservasi` (
+  `id_reservasi` int(10) unsigned NOT NULL,
+  `id_konsumsi` int(10) unsigned DEFAULT NULL,
+  `qty` int(11) DEFAULT NULL,
+  `custom` varchar(255) DEFAULT NULL,
+  `tarif` int(10) unsigned DEFAULT NULL,
+  KEY `fk_id_reservasi3` (`id_reservasi`) USING BTREE,
+  KEY `fk_id_konsumsi` (`id_konsumsi`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Table structure for tarif
--- ----------------------------
-DROP TABLE IF EXISTS `tarif`;
-CREATE TABLE `tarif` (
-`id_tarif`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`nama_tarif`  varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`nominal`  int(10) UNSIGNED NULL DEFAULT NULL ,
-PRIMARY KEY (`id_tarif`)
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=15
+-- --------------------------------------------------------
 
-;
+--
+-- Table structure for table `reservasi`
+--
 
--- ----------------------------
--- Records of tarif
--- ----------------------------
-BEGIN;
-INSERT INTO `tarif` VALUES ('12', 'Extra bed', '50000'), ('13', 'Kamar biasa', '100000'), ('14', 'Kamar VIP', '150000');
-COMMIT;
+CREATE TABLE IF NOT EXISTS `reservasi` (
+  `id_reservasi` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_kamar` int(10) unsigned DEFAULT NULL,
+  `tgl_check_in` date DEFAULT NULL,
+  `jam_check_in` time DEFAULT NULL,
+  `tgl_check_out` date DEFAULT NULL,
+  `jam_check_out` time DEFAULT NULL,
+  `temp_bayar` int(11) DEFAULT '0',
+  `status_out` tinyint(4) DEFAULT '0',
+  PRIMARY KEY (`id_reservasi`),
+  KEY `fk_id_kamar` (`id_kamar`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
 
--- ----------------------------
--- Table structure for transaksi
--- ----------------------------
-DROP TABLE IF EXISTS `transaksi`;
-CREATE TABLE `transaksi` (
-`id_transaksi`  int(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
-`id_reservasi`  int(10) UNSIGNED NULL DEFAULT NULL ,
-`id_tamu`  varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL ,
-`tgl_masuk`  date NULL DEFAULT NULL ,
-`jam_masuk`  time NULL DEFAULT NULL ,
-`tgl_keluar`  date NULL DEFAULT NULL ,
-`jam_keluar`  time NULL DEFAULT NULL ,
-`jumlah_bayar`  int(11) NULL DEFAULT NULL ,
-PRIMARY KEY (`id_transaksi`),
-FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-FOREIGN KEY (`id_tamu`) REFERENCES `tamu` (`id_tamu`) ON DELETE RESTRICT ON UPDATE RESTRICT,
-INDEX `fk_id_reservasi` (`id_reservasi`) USING BTREE ,
-INDEX `fk_id_tamu` (`id_tamu`) USING BTREE 
-)
-ENGINE=InnoDB
-DEFAULT CHARACTER SET=latin1 COLLATE=latin1_swedish_ci
-AUTO_INCREMENT=5
+--
+-- Dumping data for table `reservasi`
+--
 
-;
+INSERT INTO `reservasi` (`id_reservasi`, `id_kamar`, `tgl_check_in`, `jam_check_in`, `tgl_check_out`, `jam_check_out`, `temp_bayar`, `status_out`) VALUES
+(1, 2, '2016-07-03', '16:55:38', '0000-00-00', '16:55:45', 0, 0),
+(2, 1, '2016-07-04', '14:24:34', NULL, NULL, 0, 0);
 
--- ----------------------------
--- Records of transaksi
--- ----------------------------
-BEGIN;
-INSERT INTO `transaksi` VALUES ('1', '1', '1313', '2016-07-04', '14:21:03', null, null, '0'), ('2', '1', '1313', '2016-07-04', '14:21:30', null, null, '0'), ('3', '2', '1334', '2016-07-04', '14:26:04', null, null, '0'), ('4', '1', '12', '2016-07-04', '15:16:49', null, null, '0');
-COMMIT;
+-- --------------------------------------------------------
 
--- ----------------------------
--- View structure for transaksi_tamu
--- ----------------------------
-DROP VIEW IF EXISTS `transaksi_tamu`;
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER  VIEW `transaksi_tamu` AS SELECT
-transaksi.id_transaksi,
-transaksi.id_reservasi,
-transaksi.id_tamu,
-tamu.nama,
-tamu.alamat,
-tamu.telepon,
-reservasi.id_kamar,
-transaksi.tgl_masuk,
-transaksi.jam_masuk,
-transaksi.tgl_keluar,
-transaksi.jam_keluar,
-transaksi.jumlah_bayar
-FROM
-tamu
-INNER JOIN transaksi ON transaksi.id_tamu = tamu.id_tamu
-INNER JOIN reservasi ON transaksi.id_reservasi = reservasi.id_reservasi ;
+--
+-- Table structure for table `tamu`
+--
 
--- ----------------------------
--- Auto increment value for extra_bed
--- ----------------------------
-ALTER TABLE `extra_bed` AUTO_INCREMENT=7;
+CREATE TABLE IF NOT EXISTS `tamu` (
+  `id_tamu` varchar(50) NOT NULL,
+  `nama` varchar(50) DEFAULT NULL,
+  `alamat` varchar(255) DEFAULT NULL,
+  `telepon` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id_tamu`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- ----------------------------
--- Auto increment value for kamar
--- ----------------------------
-ALTER TABLE `kamar` AUTO_INCREMENT=5;
+--
+-- Dumping data for table `tamu`
+--
 
--- ----------------------------
--- Auto increment value for konsumsi
--- ----------------------------
-ALTER TABLE `konsumsi` AUTO_INCREMENT=3;
+INSERT INTO `tamu` (`id_tamu`, `nama`, `alamat`, `telepon`) VALUES
+('12', 'agus', 'klungkung city', '081'),
+('1313', 'Agus suarya', 'Klungkung', '085'),
+('1334', 'Putu', 'Bali', '087');
 
--- ----------------------------
--- Auto increment value for reservasi
--- ----------------------------
-ALTER TABLE `reservasi` AUTO_INCREMENT=3;
+-- --------------------------------------------------------
 
--- ----------------------------
--- Auto increment value for tarif
--- ----------------------------
-ALTER TABLE `tarif` AUTO_INCREMENT=15;
+--
+-- Table structure for table `tarif`
+--
 
--- ----------------------------
--- Auto increment value for transaksi
--- ----------------------------
-ALTER TABLE `transaksi` AUTO_INCREMENT=5;
+CREATE TABLE IF NOT EXISTS `tarif` (
+  `id_tarif` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `nama_tarif` varchar(255) DEFAULT NULL,
+  `nominal` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_tarif`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15 ;
+
+--
+-- Dumping data for table `tarif`
+--
+
+INSERT INTO `tarif` (`id_tarif`, `nama_tarif`, `nominal`) VALUES
+(12, 'Extra bed', 50000),
+(13, 'Kamar biasa', 100000),
+(14, 'Kamar VIP', 150000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaksi`
+--
+
+CREATE TABLE IF NOT EXISTS `transaksi` (
+  `id_transaksi` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `id_reservasi` int(10) unsigned DEFAULT NULL,
+  `id_tamu` varchar(50) DEFAULT NULL,
+  `tgl_masuk` date DEFAULT NULL,
+  `jam_masuk` time DEFAULT NULL,
+  `tgl_keluar` date DEFAULT NULL,
+  `jam_keluar` time DEFAULT NULL,
+  `jumlah_bayar` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id_transaksi`),
+  KEY `fk_id_reservasi` (`id_reservasi`) USING BTREE,
+  KEY `fk_id_tamu` (`id_tamu`) USING BTREE
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`id_transaksi`, `id_reservasi`, `id_tamu`, `tgl_masuk`, `jam_masuk`, `tgl_keluar`, `jam_keluar`, `jumlah_bayar`) VALUES
+(1, 1, '1313', '2016-07-04', '14:21:03', NULL, NULL, 0),
+(2, 1, '1313', '2016-07-04', '14:21:30', NULL, NULL, 0),
+(3, 2, '1334', '2016-07-04', '14:26:04', NULL, NULL, 0),
+(4, 1, '12', '2016-07-04', '15:16:49', NULL, NULL, 0);
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `transaksi_tamu`
+--
+CREATE TABLE IF NOT EXISTS `transaksi_tamu` (
+`id_transaksi` int(10) unsigned
+,`id_reservasi` int(10) unsigned
+,`id_tamu` varchar(50)
+,`nama` varchar(50)
+,`alamat` varchar(255)
+,`telepon` varchar(20)
+,`id_kamar` int(10) unsigned
+,`tgl_masuk` date
+,`jam_masuk` time
+,`tgl_keluar` date
+,`jam_keluar` time
+,`jumlah_bayar` int(11)
+);
+-- --------------------------------------------------------
+
+--
+-- Structure for view `transaksi_tamu`
+--
+DROP TABLE IF EXISTS `transaksi_tamu`;
+
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `transaksi_tamu` AS select `transaksi`.`id_transaksi` AS `id_transaksi`,`transaksi`.`id_reservasi` AS `id_reservasi`,`transaksi`.`id_tamu` AS `id_tamu`,`tamu`.`nama` AS `nama`,`tamu`.`alamat` AS `alamat`,`tamu`.`telepon` AS `telepon`,`id_kamar` AS `id_kamar`,`transaksi`.`tgl_masuk` AS `tgl_masuk`,`transaksi`.`jam_masuk` AS `jam_masuk`,`transaksi`.`tgl_keluar` AS `tgl_keluar`,`transaksi`.`jam_keluar` AS `jam_keluar`,`transaksi`.`jumlah_bayar` AS `jumlah_bayar` from ((`tamu` join `transaksi` on((`transaksi`.`id_tamu` = `tamu`.`id_tamu`))) join `reservasi` on((`transaksi`.`id_reservasi` = `id_reservasi`)));
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `extra_bed`
+--
+ALTER TABLE `extra_bed`
+  ADD CONSTRAINT `extra_bed_ibfk_1` FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`),
+  ADD CONSTRAINT `extra_bed_ibfk_2` FOREIGN KEY (`id_tarif`) REFERENCES `tarif` (`id_tarif`);
+
+--
+-- Constraints for table `kamar`
+--
+ALTER TABLE `kamar`
+  ADD CONSTRAINT `kamar_ibfk_1` FOREIGN KEY (`id_tarif`) REFERENCES `tarif` (`id_tarif`);
+
+--
+-- Constraints for table `log_reservasi`
+--
+ALTER TABLE `log_reservasi`
+  ADD CONSTRAINT `log_reservasi_ibfk_1` FOREIGN KEY (`id_konsumsi`) REFERENCES `konsumsi` (`id_konsumsi`),
+  ADD CONSTRAINT `log_reservasi_ibfk_2` FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`);
+
+--
+-- Constraints for table `reservasi`
+--
+ALTER TABLE `reservasi`
+  ADD CONSTRAINT `reservasi_ibfk_1` FOREIGN KEY (`id_kamar`) REFERENCES `kamar` (`id_kamar`);
+
+--
+-- Constraints for table `transaksi`
+--
+ALTER TABLE `transaksi`
+  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_reservasi`) REFERENCES `reservasi` (`id_reservasi`),
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_tamu`) REFERENCES `tamu` (`id_tamu`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
